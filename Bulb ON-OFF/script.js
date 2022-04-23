cbd307809f08ddaacbbd24138f7fa737fbc9e293
Abhinandan Prajapati <103814181+sdeabhi@users.noreply.github.com>
2022-04-23 22:47:17 +0530

    Add files via upload

---
+++ Bulb ON-OFF/script.js
@@ -0,0 +1,8 @@
+const onbulb = () => {
+    document.getElementById('bulb').src = 'on.png';
+}
+
+const offbulb = () => {
+    document.getElementById('bulb').src = 'off.png';
+    var a = document.querySelector('.container');
+}
